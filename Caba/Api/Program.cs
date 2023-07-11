987d6e5a77d3c3d2b28b2290808f628f44910ec4
Elias Rodriguez <elias.rodriguez@southworks.com>
2023-07-11 18:40:13 -0300

    working

---
+++ Caba/Api/Program.cs
@@ -0,0 +1,29 @@
+namespace api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Services.AddCors(options => options.AddPolicy(name: "devAllowedOrigin", policy => policy.WithOrigins("http://localhost:3000")));
+            }
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseCors("devAllowedOrigin");
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
